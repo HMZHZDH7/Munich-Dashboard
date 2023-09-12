2bb84b05ac764a249c895494510358b0f873d6a1
Gammelinne <kylian.renault@viacesi.fr>
2023-09-12 15:39:32 +0200

    few changement

--- modules/filterbyvalue_Filter.R
+++ modules/filterbyvalue_Filter.R
@@ -5,25 +5,25 @@ createFilterbyvalueUI <- function(id, suffix="") {
     filterByValueUI <- list(
         column(
             6,
-            h3(paste0("Filter by values ")), # Ajouter le suffixe au titre
+            h3(paste0("Filter by values ")), 
             sliderInput(
                 if(suffix == "_corr"){
-                     inputId = ns(paste0("slider_minmax_x", suffix)) # Ajouter le suffixe à inputId
+                     inputId = ns(paste0("slider_minmax_x", suffix)) 
                 } else {
-                   inputId = ns(paste0("slider_minmax", suffix)) # Ajouter le suffixe à inputId
+                   inputId = ns(paste0("slider_minmax", suffix))
                 },
                 label = h6("Filter values of x-axis variable (slider shows range of values in x-axis variable)"),
                 min = 0, max = 100, value = c(0, 100)
             ),
             if (suffix == "_corr") {
                 sliderInput(
-                    inputId = ns(paste0("slider_minmax_y", suffix)), # Ajouter le suffixe à inputId
+                    inputId = ns(paste0("slider_minmax_y", suffix)), 
                     label = h6("Filter values of y-axis variable (slider shows range of values in y-axis variable)"),
                     min = 0, max = 100, value = c(0, 100)
                 )
             },
             checkboxGroupInput(
-                inputId = ns(paste0("selected_quarts", suffix)), # Ajouter le suffixe à inputId
+                inputId = ns(paste0("selected_quarts", suffix)), 
                 label = h6("Quarters shown in plot:"),
                 choices = c(
                     "2018 Q1", "2018 Q2", "2018 Q3", "2018 Q4",
